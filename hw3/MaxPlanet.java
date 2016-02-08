54713498d20321fc6bcf16e71591c4b1b309280f
Qianyi Guo <guoqianyi@gmail.com>
2016-02-08 22:27:51 +0800

    hw3-4

--- hw3/MaxPlanet.java
+++ hw3/MaxPlanet.java
@@ -1,5 +1,5 @@
 import java.util.Comparator;
-/* Import anything else you need here. */
+import java.util.Arrays;
 
 /**
  * MaxPlanet.java
@@ -9,6 +9,8 @@ public class MaxPlanet {
 
     /** Returns the Planet with the maximum value according to Comparator c. */
     public static Planet maxPlanet(Planet[] planets, Comparator<Planet> c) {
-        // REPLACE THIS LINE WITH YOUR SOLUTION
+        Arrays.sort(planets, c);
+        int N = planets.length;
+        return planets[N - 1];
     }
 }
