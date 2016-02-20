b976a8a551991754e1c5b5e52d72dd162a8bbbf9
Qianyi Guo <guoqianyi@gmail.com>
2016-02-20 16:41:34 +0800

    dis9-1

--- dis9/BSTNode.java
+++ dis9/BSTNode.java
@@ -36,6 +36,23 @@ public class BSTNode {
 		return result;
 	}
 
+	/* Returns the BSTNode that is the shortest common ancestor of n1 and n2. */
+	public static BSTNode commonAncestor(BSTNode root, int n1, int n2) {
+		if (n1 > n2) {
+			return null;
+		}
+		if (n1 <= root.value && n2 >= root.value) {
+			return root;
+		}
+		if (n1 > root.value) {
+			return commonAncestor(root.right, n1, n2);
+		}
+		if (n2 < root.value) {
+			return commonAncestor(root.left, n1, n2);
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
 		int[] arr = {1, 2, 3, 4, 5, 6, 7};
 		BSTNode t = BSTNode.makeBST(arr);
@@ -46,5 +63,13 @@ public class BSTNode {
 		System.out.println(t.right.value);
 		System.out.println(t.right.left.value);
 		System.out.println(t.right.right.value);
+
+		System.out.println("------------------------------");
+
+		System.out.println(commonAncestor(t, 1, 3).value);
+		System.out.println(commonAncestor(t, 5, 7).value);
+		System.out.println(commonAncestor(t, 1, 7).value);
+		System.out.println(commonAncestor(t, 2, 6).value);
+		System.out.println(commonAncestor(t, 5, 2));
 	}
 }
