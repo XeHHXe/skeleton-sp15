8dc7b98abc34ffce38258d6683c767e81dd6a3ab
Qianyi Guo <guoqianyi@gmail.com>
2016-03-28 16:00:54 +0800

    hw7-0

--- hw7/Card.java
+++ hw7/Card.java
@@ -26,14 +26,16 @@ public class Card {
     @Override
     public boolean equals(Object o) {
         if (o instanceof Card) {
-            // YOUR CODE HERE 
+            Card c = (Card) o;
+            if (c.suit == this.suit && c.number == this.number) {
+                return true;
+            }
         }
         return false;
     }
 
     @Override
     public int hashCode() {
-        // YOUR CODE HERE (this is possible in one concise line)
-        return 0;
+        return suit * 100 + number;
     }
 }
