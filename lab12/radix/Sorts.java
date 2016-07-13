dfc939355cf2cb1eb89ae253d05103e4b35295c2
Qianyi Guo <guoqianyi@gmail.com>
2016-07-13 23:56:59 +0800

    lab12-1

--- lab12/radix/Sorts.java
+++ lab12/radix/Sorts.java
@@ -8,7 +8,7 @@ package radix;
  */
 public class Sorts {
 
-
+    private static final int BASE = 16;
     /**
      *  Sorts an array of int keys according to the values of <b>one</b>
      *  of the base-16 digits of each key. Returns a <b>NEW</b> array and
@@ -22,7 +22,31 @@ public class Sorts {
      *    and containing the same keys sorted according to the chosen digit.
      **/
     public static int[] countingSort(int[] keys, int whichDigit) {
-        //YOUR CODE HERE
+        int[] result = new int[keys.length];
+        int[] count = new int[BASE];
+        int i, j;
+        int radix = 1;
+        for (i = 0; i < whichDigit; i++) {
+            radix *= BASE;
+        }
+
+        for (i = 0; i < BASE; i++) {
+            count[i] = 0; // initialize the count
+        }
+        for (i = 0; i < keys.length; i++) {
+            j = (keys[i] / radix) % BASE;
+            count[j]++; // count elements in each bucket
+        }
+        for (i = 0; i < BASE; i++) {
+            count[i] = count[i - 1] + count[i]; 
+            // allocate positions in result for each bucket
+        }
+        for (i = BASE - 1; i >= 0; i--) {
+            j = (keys[i] / radix) % BASE;
+            result[count[j] - 1] = keys[i];
+            count[j]--;
+        }
+        return result;
     }
 
     /**
@@ -34,7 +58,16 @@ public class Sorts {
      *    and containing the same keys in sorted order.
      **/
     public static int[] radixSort(int[] keys) {
-        //YOUR CODE HERE
+        int[] result = new int[keys.length];
+        int i, j;
+        for (i = 0; i < keys.length; i++) {
+            result[i] = keys[i];
+        }
+        for (j = 0; j < 8; j++) {
+            result = countingSort(result, j);
+            // sort from least to most significant digit
+        }
+        return result;
     }
 
 }
